00c226c685c623d0fc365b2cedd287f713d39fd7
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-11-07 14:26:57 +0100

    Validates RESTKIT_ROOT

--- cmd/init.go
+++ cmd/init.go
@@ -1,8 +1,13 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"strings"
 )
 
 var name string
@@ -11,7 +16,32 @@ var initCmd = &cobra.Command{
 	Short: "Creates the project",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Creating project '%s'\n", args[0])
+		root := viper.GetString("RESTKIT_ROOT")
+		if len(root) == 0 {
+			fmt.Fprint(os.Stderr, "env RESTKIT_ROOT not set")
+			os.Exit(1)
+		}
+
+		if _, err := os.Stat(root); errors.Is(err, os.ErrNotExist) {
+			fmt.Fprintf(os.Stderr, "Root directory %s does not exist", root)
+			os.Exit(1)
+		}
+
+		if !strings.HasSuffix(root, string(os.PathSeparator)) {
+			root = fmt.Sprintf("%s%s", root, string(os.PathSeparator))
+		}
+
+		log.Printf("RESTKIT_ROOT: %s\n", root)
+		path := root + args[0]
+		if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+			log.Printf("Preating project '%s'...\n", path)
+			err := os.Mkdir(path, os.ModePerm)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			log.Printf("Project '%s' exists\n", path)
+		}
 	},
 }
 
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import "github.com/rwirdemann/restkit/cmd"
+import "github.com/spf13/viper"
 
 func main() {
+	viper.BindEnv("RESTKIT_ROOT")
+
 	cmd.Execute()
 }
