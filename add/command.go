81076990ccadc3c2f062ca83d704094a69f2cc8b
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-11-14 11:52:24 +0100

    Inserts import and http handler into main

--- add/command.go
+++ add/command.go
@@ -3,8 +3,10 @@ package add
 import (
 	"fmt"
 	"github.com/rwirdemann/restkit/io"
+	"github.com/spf13/viper"
 	"log"
 	"os"
+	"os/exec"
 	"text/template"
 	"unicode"
 )
@@ -37,13 +39,34 @@ func Execute(resourceName string) error {
 	}
 
 	// Insert create http handler fragment into main file
+	log.Printf("insert: %s...ok\n", "import")
 	err = io.InsertFragment("main.go",
-		"err := http.ListenAndServe(fmt.Sprintf(\":%s\", \"8080\"), router)",
-		"    createHandler()")
+		"\"net/http\"",
+		"http2 \"github.com/rwirdemann/bookstore/adapter/http\"")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	log.Printf("insert: %s...ok\n", "http handler")
+	err = io.InsertFragment("main.go",
+		"err := http.ListenAndServe(fmt.Sprintf(\":%s\", \"8080\"), router)",
+		"bookAdapter := http2.NewBookHandler()\n"+
+			"\trouter.HandleFunc(\"/books\", bookAdapter.GetAll()).Methods(\"GET\")\n")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	root := viper.GetString("RESTKIT_ROOT")
+	if len(root) == 0 {
+		return fmt.Errorf("env %s not set", "RESTKIT_ROOT")
+	}
+	path := fmt.Sprintf("%s/bookstore", root)
+	cmd := fmt.Sprintf("go fmt %s", path)
+
+	_, err = exec.Command("bash", "-c", cmd).Output()
+	if err != nil {
+		log.Fatalln(err)
+	}
 	return nil
 }
 
