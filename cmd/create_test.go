da80aa9007656abf99ac340ea8563f87c87650e9
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-11-15 11:58:07 +0100

    Adds first mockery based test

--- cmd/create_test.go
+++ cmd/create_test.go
@@ -1,7 +1,14 @@
 package cmd
 
-import "testing"
+import (
+	"github.com/rwirdemann/restkit/mocks/github.com/rwirdemann/restkit/ports"
+	"testing"
+)
 
 func TestCreateProjectDirectory(t *testing.T) {
+	mockEnv := ports.NewMockEnv(t)
+	env = mockEnv
+	mockEnv.EXPECT().RKRoot().Return("/github.com/rwirdemann", nil)
 
+	create("bookstore")
 }
