64234b6b80e24cc96ace0abc24934fe889866a5e
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-11-16 10:37:41 +0100

    Add more expectations to create_test.go

--- cmd/create_test.go
+++ cmd/create_test.go
@@ -10,11 +10,22 @@ func TestCreateProjectDirectory(t *testing.T) {
 	env = mockEnv
 	mockFileSystem := ports.NewMockFileSystem(t)
 	fileSystem = mockFileSystem
+	mockTemplate := ports.NewMockTemplate(t)
+	template = mockTemplate
 
 	mockEnv.EXPECT().RKRoot().Return("/github.com/rwirdemann/", nil)
 	mockFileSystem.EXPECT().Exists("/github.com/rwirdemann/bookstore").Return(false)
 	mockFileSystem.EXPECT().CreateDir("/github.com/rwirdemann/bookstore").Return(nil)
 	mockFileSystem.EXPECT().CreateFile("/github.com/rwirdemann/bookstore/.restkit").Return(nil, nil)
+
+	path := "/github.com/rwirdemann/bookstore"
+	data := struct {
+		Project string
+	}{
+		Project: "bookstore",
+	}
+	mockTemplate.EXPECT().Create("go.mod.txt", "go.mod", path, data).Return(nil)
+	mockTemplate.EXPECT().Create("main.go.txt", "main.go", path, data).Return(nil)
 
 	create("bookstore")
 }
